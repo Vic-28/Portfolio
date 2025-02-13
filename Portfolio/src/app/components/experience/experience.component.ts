2f470a52997293f6dbd27a729d6c5e31cb9dec38
Víctor Delgado Batista <delgadobatistavictor@gmail.com>
2025-02-13 00:01:08 +0000

    feature: add ademypro experience

--- Portfolio/src/app/components/experience/experience.component.ts
+++ Portfolio/src/app/components/experience/experience.component.ts
@@ -7,20 +7,40 @@ import { Component } from '@angular/core';
 })
 export class ExperienceComponent {
 
-  experiencia =[
+  experiencia = [
     {
-      title:'Desarrollador Junior Full Stack - Intern',
-      company:'Eviden/Atos',
-      date:'Marzo 2024 - Junio 2024',
-      details:[
-        'Liderar un pequeño equipo en proyectos internos de desarrollo web con metodologías ágiles.',
-        'Implementación de prácticas de código limpio y aseguramiento de la calidad con herramientas como SonarQube.',
-        'Uso de Git para el control de versiones y la colaboración en el desarrollo de software.',
-        'Integración continua y despliegue continuo (CI/CD) para automatizar y mejorar los procesos de desarrollo y lanzamiento de aplicaciones.'
+      title: 'Desarrollador Web',
+      company: 'AdemyPro',
+      date: 'Diciembre 2024 - Febrero 2025',
+      details: [
+        'Proporcioné asistencia técnica.',
+        'Gestioné servicios web.',
+        'Desarrollé herramientas orientadas a la empresa. (ERP)',
+        'Implementé integración continua y despliegue continuo (CI/CD) para automatizar y mejorar los procesos de desarrollo y lanzamiento de aplicaciones.'
+      ],
+      utilitys: [
+        'Sonar',
+        'Git',
+        'Angular',
+        'TypeScript',
+        'Java',
+        'BootStrap',
+        'Asistencia Técnica',
+        'Gestión de Servicios Web',
+        'Redes'
       ]
-      ,
-      utilitys:
-      [
+    },
+    {
+      title: 'Desarrollador Junior Full Stack - Intern',
+      company: 'Eviden/Atos',
+      date: 'Marzo 2024 - Junio 2024',
+      details: [
+        'Lideré un pequeño equipo en proyectos internos de desarrollo web utilizando metodologías ágiles.',
+        'Implementé prácticas de código limpio y aseguramiento de la calidad con herramientas como SonarQube.',
+        'Utilicé Git para el control de versiones y la colaboración en el desarrollo de software.',
+        'Implementé integración continua y despliegue continuo (CI/CD) para automatizar y mejorar los procesos de desarrollo y lanzamiento de aplicaciones.'
+      ],
+      utilitys: [
         'Sonar',
         'Git',
         'Angular',
@@ -29,31 +49,29 @@ export class ExperienceComponent {
         'Agile',
         'TypeScript',
         'Java',
-        'BootStrap',
+        'BootStrap'
       ]
     },
     {
-      title:'Técnico Informático - Intern',
-      company:'Ayuntamiento de Granadilla de Abona',
-      date:'Marzo 2022 - Junio 2022',
-      details:[
-        'Diagnóstico y reparación de hardware y software en equipos de escritorio y portátiles.',
-        'Mantenimiento preventivo y correctivo para asegurar la operatividad continua de los dispositivos.',
-        'Solución de problemas de red y gestión de recursos de red como routers, switches y firewalls.',
-        'Gestión de servidores.'
-      ]
-      ,
-      utilitys:
-      [
+      title: 'Técnico Informático - Intern',
+      company: 'Ayuntamiento de Granadilla de Abona',
+      date: 'Marzo 2022 - Junio 2022',
+      details: [
+        'Diagnostiqué y reparé hardware y software en equipos de escritorio y portátiles.',
+        'Realicé mantenimiento preventivo y correctivo para asegurar la operatividad continua de los dispositivos.',
+        'Resolví problemas de red y gestioné recursos de red como routers, switches y firewalls.',
+        'Gestioné servidores.'
+      ],
+      utilitys: [
         'Windows Server',
         'Linux',
         'Linux Server',
         'Redes',
         'Hardware',
         'Software',
-        'Mantenimiento',
+        'Mantenimiento'
       ]
     }
-  ]
+  ];
 
 }
