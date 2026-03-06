317abbeeb07a3136267689f03bdcc32e5c49346c
Víctor Delgado Batista <delgadobatistavictor@gmail.com>
2026-03-06 15:38:31 +0000

    first commit de V2

---
+++ Portfolio_V2/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Dashboard from './Components/Dashboard/dashboard'
+
+function App() {
+
+  return (
+    <>
+      <Dashboard />
+    </>
+  )
+}
+
+export default App
---
+++ Portfolio_V2/src/Components/Dashboard/dashboard.tsx
@@ -0,0 +1,12 @@
+import './dashboard.css'
+
+function Dashboard() {
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default Dashboard
