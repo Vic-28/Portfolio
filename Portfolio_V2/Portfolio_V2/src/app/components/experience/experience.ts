814b13c0a62bcde0ae9e02c24b4f82f35cd997a3
Víctor Delgado Batista <delgadobatistavictor@gmail.com>
2026-04-21 13:22:59 +0100

    Cambio en descripción de experiencia

--- Portfolio_V2/Portfolio_V2/src/app/components/experience/experience.ts
+++ Portfolio_V2/Portfolio_V2/src/app/components/experience/experience.ts
@@ -24,21 +24,21 @@ export class Experience {
       logo: "https://i.postimg.cc/QxXsMLCL/27-logo-iter.png",
       role: 'Desarrollador Web',
       period: 'Abril 2025 - Abril 2026',
-      description: 'Pequeña descripciíon del puesto en ITER.',
+      description: 'Desarrollo y mantenimiento de una plataforma interna de compras orientada a optimizar el flujo operativo y financiero de la organización. Participación en la implementación de funcionalidades para la gestión centralizada de información económica y la mejora de la trazabilidad de los procesos de compra.',
     },
     {
       company: 'Ademy',
       logo: "https://i.postimg.cc/Jhx9mPY2/edificio-de-oficinas.png",
       role: 'Desarrollador Web',
       period: 'Abril 2025 - Abril 2026',
-      description: 'Pequeña descripciíon del puesto en ITER.',
+      description: 'Desarrollador Full Stack especializado en Angular y Spring Boot, con experiencia en diseño de aplicaciones escalables, desarrollo de APIs REST y liderazgo de equipos en entornos ágiles. Comprometido con la calidad del código y la mejora continua.',
     },
     {
       company: 'Eviden',
       logo: "https://i.postimg.cc/YCqPR1C9/Eviden.jpg",
       role: 'Desarrollador Web',
       period: '2022 - 2024',
-      description: 'Descripción de las responsabilidades y logros en este puesto.',
+      description: 'Especialista en soporte IT con experiencia en administración de servidores, redes y conectividad, resolución de incidencias y formación a usuarios. Conocimientos en hardware, software y ciberseguridad, con enfoque en mantenimiento y optimización de sistemas.',
     },
   ]);
 
